2fae7d92f3e62cbe15521433d5c3fc70a14ca37e
Vinhadi10 <arvianh29@gmail.com>
2022-01-22 12:47:27 +0800

    model

--- app/Models/camp.php
+++ app/Models/camp.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-class camp extends Model
+
+class Camp extends Model
 {
     use HasFactory, SoftDeletes;
     protected $fillable = ['title', 'price'];
